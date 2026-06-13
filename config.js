ead2bd93ff3bce0521cf4a6d0f546be793ff86ae
schuwaong <43778350+schuwaong@users.noreply.github.com>
2026-06-13 21:24:00 +0800

    Align hosted female image generation limit

--- config.js
+++ config.js
@@ -2,6 +2,10 @@
   const host = window.location.hostname;
   const isLocalHost = host === "localhost" || host === "127.0.0.1" || host === "";
 
+  if (window.IC_FEMALE_IMAGE_GENERATION_LIMIT === undefined) {
+    window.IC_FEMALE_IMAGE_GENERATION_LIMIT = 1;
+  }
+
   if (!isLocalHost) {
     window.IC_BACKEND_BASE_URL = window.IC_BACKEND_BASE_URL || "https://ic-wearables.vercel.app";
   }
